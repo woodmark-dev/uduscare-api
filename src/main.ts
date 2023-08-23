daaf7e4c0fc73855baa375ee878a98bb99bf65e8
woodmark-dev <ibrahimayuba393@gmail.com>
2023-08-23 15:55:42 +0100

    fixing

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({ credentials: true, origin: ["https://uduscare.onrender.com/"] });
+  app.enableCors({ credentials: true, origin: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3333);
 }
