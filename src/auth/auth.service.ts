b8e53f4dc05f5d0e67d790779ca8d347066f353b
woodmark-dev <ibrahimayuba393@gmail.com>
2023-08-23 15:45:17 +0100

    fixing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -127,7 +127,7 @@ export class AuthService {
     });
 
     response.set({
-      'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Origin': "*", "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, UPDATE",
+      'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Origin': "https://uduscare.onrender.com/", "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, UPDATE",
     })
 
     return { message: user.role, statusCode: HttpStatus.CREATED };
